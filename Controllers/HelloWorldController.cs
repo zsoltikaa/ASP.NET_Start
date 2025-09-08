de94f52fcb40a2c3478acd1d47860b86c7c239bc
zsoltikaa <padarzsolti@gmail.com>
2025-09-08 12:15:49 +0200

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovies.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //GET: HelloWorld/Index
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // GET: HelloWorld/Welcome
+    public IActionResult Welcome(string name, int number = 1)
+    {
+
+        ViewData["Message"] = $"Hello {name}!";
+        ViewData["Number"] = number;
+
+        return View();
+
+        //return HtmlEncoder.Default.Encode($"Hello {name}, age is: {age}");
+    }
+
+    /* Custom URL routing example:
+     * https://localhost:7256/HelloWorld/Welcome/?name=Zsoltika&age=19
+     *                                           query string (modal binding)
+     */
+        
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovies.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MvcMoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMoviesContext") ?? throw new InvalidOperationException("Connection string 'MvcMoviesContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
